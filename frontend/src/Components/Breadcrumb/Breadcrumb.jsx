3681c4e32324a6e0c7e0aaf944caa4e083c05e62
keong <keong_97@live.com.my>
2024-10-22 15:39:28 +0800

    [2.0.1] Format with prettier formatter
    Format with prettier formatter

--- frontend/src/Components/Breadcrumb/Breadcrumb.jsx
+++ frontend/src/Components/Breadcrumb/Breadcrumb.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './Breadcrumb.css'
-import arrow_icon from '../Assets/breadcrumb_arrow.png'
+import React from "react";
+import "./Breadcrumb.css";
+import arrow_icon from "../Assets/breadcrumb_arrow.png";
 
 export const Breadcrumb = (props) => {
-    const {product} = props;
+  const { product } = props;
   return (
-    <div className='breadcrumb'>
-        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+    <div className="breadcrumb">
+      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />{" "}
+      {product.category} <img src={arrow_icon} alt="" /> {product.name}
     </div>
-  )
-}
+  );
+};
